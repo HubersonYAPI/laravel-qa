c8b822f5523dbefc6f63a7fb1ff76b74546b8b73
Huberson Yapi <hubersonyapi@yahoo.fr>
2021-05-03 22:05:29 +0000

    Affichage des reponses d'une question

--- app/Answer.php
+++ app/Answer.php
@@ -20,4 +20,23 @@ class Answer extends Model
 
         return \Parsedown::instance()->text($this->body);
     }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function($answer){
+            echo "Response créée\n";
+        });
+
+        static::saved(function($answer){
+            echo "Response enregistrée\n";
+        });
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+        // return $this->created_at->format("d/m/Y");
+    }
 }
