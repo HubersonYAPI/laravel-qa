56eddb7acc7cd35bb5452ed242bb06116fdcdfef
Huberson Yapi <hubersonyapi@yahoo.fr>
2021-05-03 19:38:53 +0000

    Creation de la tabe answsers et modifications du champ answers = answers_count dans la table question

--- app/Question.php
+++ app/Question.php
@@ -36,13 +36,20 @@ class Question extends Model
 
     public function getStatusAttribute()
     {
-        if ($this->answers > 0) {
+        if ($this->answers_count > 0) {
             if ($this->best_answer_id) {
                 return "answered-accepted";
             }
             return "answered";
         }
         return "unanswered";
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+        //$question->answers->count()
+        //foreach ($question->answers as answer)
     }
 
 
